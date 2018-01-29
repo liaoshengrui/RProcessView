ff35093cd7dc948daa58c09d6102f5bd4eba8cad
liaoshengrui <471406763@qq.com>
2018-01-29 17:51:11 +0800

    新增自定义属性方式设置

--- app/src/main/java/com/ui/liaoshengrui/rprocess/MainActivity.java
+++ app/src/main/java/com/ui/liaoshengrui/rprocess/MainActivity.java
@@ -2,15 +2,36 @@ package com.ui.liaoshengrui.rprocess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
     RProcessView rProcess;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rProcess=findViewById(R.id.rprocess);
+        rProcess = findViewById(R.id.rprocess);
+        new Thread() {
+            @Override
+            public void run() {
+                int process = rProcess.getProcess();
+
+                while (process < 100) {
+                    process++;
+                    rProcess.setProcess(process);
+                    Log.i("tag", "当前进度" + process);
+                    try {
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/ui/liaoshengrui/rprocess/RProcessView.java
+++ app/src/main/java/com/ui/liaoshengrui/rprocess/RProcessView.java
@@ -1,11 +1,13 @@
 package com.ui.liaoshengrui.rprocess;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -15,13 +17,13 @@ import android.view.View;
 
 public class RProcessView extends View {
 
-    private Paint backPint;
-    private Paint proPaint;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private volatile String text;
-    private float textSize = 40;
+    private float textSize;
+    private float radius;
     private volatile int process = 0;
-    private int mainColor = Color.BLUE;
-    private int textColor = Color.WHITE;
+    private int mainColor;
+    private int textColor;
 
     public RProcessView(Context context) {
         this(context, null);
@@ -33,11 +35,22 @@ public class RProcessView extends View {
 
     public RProcessView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        backPint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        backPint.setColor(mainColor);
-        backPint.setTextSize(textSize);
-        proPaint = new Paint(backPint);
-        text = "暂停";
+        initAttrs(attrs);
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.RProcessView);
+        text = ta.getString(R.styleable.RProcessView_text);
+        if (TextUtils.isEmpty(text)) {
+            text = "暂停";
+        }
+        textSize = ta.getDimension(R.styleable.RProcessView_textSize, 30);
+        radius = ta.getFloat(R.styleable.RProcessView_radius, 15);
+        mainColor = ta.getColor(R.styleable.RProcessView_mainColor, Color.BLUE);
+        textColor = ta.getColor(R.styleable.RProcessView_textColor, Color.WHITE);
+
+        mPaint.setTextSize(textSize);
+
     }
 
     @Override
@@ -46,28 +59,30 @@ public class RProcessView extends View {
         int width = getLayoutParams().width;
         int height = getLayoutParams().height;
 
-        float textLenght = backPint.measureText(text);
+        float textLenght = mPaint.measureText(text);
         float startX = (width - textLenght) / 2;
-        float startY = getTextHight(backPint);
+        float startY = getTextHight(mPaint);
 
-        backPint.setStyle(Paint.Style.STROKE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(mainColor);
         RectF rectF = new RectF(0, 0, width, height);
-        canvas.drawRoundRect(rectF, 10, 10, backPint);
-        canvas.drawText(text, startX, startY, backPint);
+        canvas.drawRoundRect(rectF, radius, radius, mPaint);
+        canvas.drawText(text, startX, startY, mPaint);
 
         canvas.save();
+        mPaint.setStyle(Paint.Style.FILL);
         float clipW = (float) (process * 0.01 * width);
         RectF clipRectF = new RectF(0, 0, clipW, height);
         canvas.clipRect(clipRectF);
-        proPaint.setColor(mainColor);
-        canvas.drawRoundRect(rectF, 10, 10, proPaint);
-        proPaint.setColor(textColor);
-        canvas.drawText(text, startX, startY, proPaint);
+        mPaint.setColor(mainColor);
+        canvas.drawRoundRect(rectF, radius, radius, mPaint);
+        mPaint.setColor(textColor);
+        canvas.drawText(text, startX, startY, mPaint);
         canvas.restore();
     }
 
 
-    public void setProcess(int process) throws Exception {
+    public void setProcess(int process) {
         if (process < 0 || process > 100) {
             return;
         }
@@ -80,8 +95,8 @@ public class RProcessView extends View {
         return process;
     }
 
-    public void setText(String text){
-        this.text=text;
+    public void setText(String text) {
+        this.text = text;
         postInvalidate();
     }
 
